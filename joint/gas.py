1ab63bf5a32d63567630f1cad13a05128f8606f7
Ian Czekala <iancze@gmail.com>
2019-10-09 23:04:28 -0700

    added FITS file.

--- joint/gas.py
+++ joint/gas.py
@@ -21,25 +21,29 @@ def load_data(fname, mu_RA, mu_DEC):
     nx = header["NAXIS1"]
     ny = header["NAXIS2"]
 
-    assert nx % 2 == 0 and ny % 2 == 0 , "We don't have an even number of pixels, assumptions in the routine are violated."
+    assert (
+        nx % 2 == 0 and ny % 2 == 0
+    ), "We don't have an even number of pixels, assumptions in the routine are violated."
 
     # RA coordinates
-    CDELT1 = header["CDELT1"] # decimal degrees. Note that not all decimal degrees are the same distance on the sky
+    CDELT1 = header[
+        "CDELT1"
+    ]  # decimal degrees. Note that not all decimal degrees are the same distance on the sky
     # since this depends on what declination we're at, too!
-    CRPIX1 = header["CRPIX1"] - 1. # Now indexed from 0
+    CRPIX1 = header["CRPIX1"] - 1.0  # Now indexed from 0
     # DEC coordinates
     CDELT2 = header["CDELT2"]
-    CRPIX2 = header["CRPIX2"] - 1. # Now indexed from 0
-    cosdec = np.cos(CDELT2 * np.pi/180)
+    CRPIX2 = header["CRPIX2"] - 1.0  # Now indexed from 0
+    cosdec = np.cos(CDELT2 * np.pi / 180)
 
-    dRAs = (np.arange(nx) - nx/2) * CDELT1 * cosdec
-    dDECs = (np.arange(ny) - ny/2) * CDELT2
+    dRAs = (np.arange(nx) - nx / 2) * CDELT1 * cosdec
+    dDECs = (np.arange(ny) - ny / 2) * CDELT2
 
-    RA = 3600 * dRAs # [arcsec]
-    DEC = 3600 * dDECs # [arcsec]
+    RA = 3600 * dRAs  # [arcsec]
+    DEC = 3600 * dDECs  # [arcsec]
 
     # apply the centroid shift
-    ext = (RA[0] - mu_RA, RA[-1] - mu_RA, DEC[0] - mu_DEC, DEC[-1] - mu_DEC) # [arcsec]
+    ext = (RA[0] - mu_RA, RA[-1] - mu_RA, DEC[0] - mu_DEC, DEC[-1] - mu_DEC)  # [arcsec]
 
     # Get the beam info from the header, like normal
     BMAJ = 3600 * header["BMAJ"]
@@ -51,13 +55,22 @@ def load_data(fname, mu_RA, mu_DEC):
     return (data, beam, ext)
 
 
-def plot_beam(ax, beam, xy=(1,-1), facecolor="0.5", edgecolor="0.5"):
+def plot_beam(ax, beam, xy=(1, -1), facecolor="0.5", edgecolor="0.5"):
     BMAJ, BMIN, BPA = beam
     # print('BMAJ: {:.3f}", BMIN: {:.3f}", BPA: {:.2f} deg'.format(BMAJ, BMIN, BPA))
     # We need to negate the BPA since the rotation is the opposite direction
     # due to flipping RA.
-    ax.add_artist(Ellipse(xy=xy, width=BMIN, height=BMAJ, angle=-BPA, facecolor=facecolor, linewidth=0.2, edgecolor=edgecolor))
-
+    ax.add_artist(
+        Ellipse(
+            xy=xy,
+            width=BMIN,
+            height=BMAJ,
+            angle=-BPA,
+            facecolor=facecolor,
+            linewidth=0.2,
+            edgecolor=edgecolor,
+        )
+    )
 
 
 def plot_gas(ax):
@@ -66,11 +79,11 @@ def plot_gas(ax):
     """
 
     # shift the image by setting the axes limits
-    mu_RA = (525 - 478.768 ) * 0.015 # x
-    mu_DEC = (538.165 - 525) * 0.015 # y from 0, 0
+    mu_RA = (525 - 478.768) * 0.015  # x
+    mu_DEC = (538.165 - 525) * 0.015  # y from 0, 0
     # center pixel is 525, 525
 
-    radius = 3.0 # arcsec
+    radius = 3.0  # arcsec
 
     data, beam, ext = load_data("gas.fits", mu_RA, mu_DEC)
 
